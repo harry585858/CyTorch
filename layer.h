1c6ed0427804ecc1f9d6ca9a5563fbcea42a1ad7
Y <87462160+harry585858@users.noreply.github.com>
2025-05-14 20:56:44 +0900

    Update layer.h

--- layer.h
+++ layer.h
@@ -4,19 +4,20 @@
 // 공통 기본 클래스
 class Layer {
 public:
+    std::vector<vertex> varr;
     Layer* front;
     Layer* rear;
-
     virtual ~Layer() = default;  // 소멸자에 virtual 추가
-
     // 다형성을 위한 virtual 함수들
     virtual void print() = 0;
     virtual void printrear() = 0;
+    void runlayer();
 };
 
 // 템플릿 클래스 (구체적인 Layer 타입)
 class Layer_4 : public Layer {
 public:
+    float (Layer::*fp)(float);
     std::vector<vertex_4> varr;
 
     Layer_4(int size);
@@ -30,10 +31,26 @@ public:
 
 class Layer_8 : public Layer {
 public:
+    double (Layer::*fp)(double);
     std::vector<vertex_8> varr;
 
     Layer_8(int size);
 
+    void print() override;
+
+    void printrear() override;
+
+    void append(Layer_8* toappend);
+};
+class Endlayer : public Layer{
+public: 
+    double (Layer::*fp)(double);
+    std::vector<vertex_8> varr;
+
+    Endlayer(int size);
+    
+    void runlayer();
+    
     void print() override;
 
     void printrear() override;
