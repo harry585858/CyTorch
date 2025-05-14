d50bcca0b2ab67497155b3c917f80262c17a8131
Y <87462160+harry585858@users.noreply.github.com>
2025-05-14 20:56:17 +0900

    Update layer.cpp

--- layer.cpp
+++ layer.cpp
@@ -1,6 +1,14 @@
 #include "layer.h"
-#include <iostream>
+#include <cstdio>
 // 템플릿 구현
+void Layer::runlayer(){
+    for(int i=0;i<this->varr.size(); i++){
+        auto output = varr[i].runvertex();
+        for(int j = 0; j < this->rear->varr.size(); j++){//next input
+            this->rear->varr[j].weight[i] = output;
+        }
+    }
+}
 Layer_4::Layer_4(int size) {
     front = nullptr;
     rear = nullptr;
@@ -11,7 +19,7 @@ void Layer_4::append(Layer_4* toappend) {
     toappend->rear = this->rear;
     this->rear = toappend;
     for(int i=0;i<toappend->varr.size(); i++){
-        toappend->varr[i].weight.resize(this->varr.size(),1);
+        toappend->varr[i].weight.resize(this->varr.size());
     }
 }
 
@@ -30,7 +38,6 @@ void Layer_4::print() {
     }
     std::cout << std::endl;
 }
-
 // 명시적 인스턴스화: Layer 클래스가 float, double을 처리할 수 있도록 인스턴스화
 Layer_8::Layer_8(int size) {
     front = nullptr;
@@ -41,9 +48,6 @@ void Layer_8::append(Layer_8* toappend) {
     toappend->front = this;
     toappend->rear = this->rear;
     this->rear = toappend;
-    for(int i=0;i<toappend->varr.size(); i++){
-        toappend->varr[i].weight.resize(this->varr.size(),1);
-    }
 }
 
 void Layer_8::printrear() {
@@ -60,4 +64,33 @@ void Layer_8::print() {
         varr[i].print();
     }
     std::cout << std::endl;
+}
+Endlayer::Endlayer(int size){
+    front = nullptr;
+    rear = nullptr;
+    varr.resize(size);
+}
+void Endlayer::printrear() {
+    Layer* start = this;
+    while (start != nullptr) {
+        start->print();
+        start = static_cast<Layer*>(start->rear);
+    }
+}
+
+void Endlayer::print() {
+    for (int i = 0; i < varr.size(); i++) {
+        std::cout << "vertex : " << i << std::endl;
+        varr[i].print();
+    }
+    
+    std::cout << std::endl;
+}
+void Endlayer::runlayer(){
+    this->print();
+    printf("---THE END!---\n");
+    for(int i=0;i<this->varr.size(); i++){
+        auto output = varr[i].runvertex();
+        printf("%lf ", output);
+    }
 }
